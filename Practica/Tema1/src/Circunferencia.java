883bffacb12ae2f47a053949317103e04c340c55
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-08-09 12:02:17 -0300

    TP1 Act 3

---
+++ Practica/Tema1/src/Circunferencia.java
@@ -0,0 +1,8 @@
+public class Circunferencia {
+    public static void main(String[] args) {
+        int radio;
+        radio = 19;
+
+        System.out.println("Perímetro: " + (Math.PI * Math.pow(radio, 2)));
+    }
+}
