08be869aa8ad3c1ea2746141f1377e7c99d08a72
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-08-09 19:31:42 -0300

    Update TrianguloRectangulo.java

--- Practica/Tema1/src/TrianguloRectangulo.java
+++ Practica/Tema1/src/TrianguloRectangulo.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class TrianguloRectangulo {
     public static void main(String[] args) {
-        Scanner num = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
 
         System.out.print("Ingresar h: ");
-        double h = num.nextDouble();
+        double h = teclado.nextDouble();
         System.out.print("Ingresar c1: ");
-        double c1 = num.nextDouble();
+        double c1 = teclado.nextDouble();
         System.out.print("Ingresar c2: ");
-        double c2 = num.nextDouble();
+        double c2 = teclado.nextDouble();
 
         if (Math.pow(h, 2) == Math.pow(c1, 2) + Math.pow(c2, 2)) {
             System.out.println("Es un triangulo rectangulo");
