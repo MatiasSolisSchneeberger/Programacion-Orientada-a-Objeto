40f98aa246d903362a8b5410fab1d3fb22584ef8
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-08-09 19:31:18 -0300

    TP 1 Act 7

---
+++ Practica/Tema1/src/CircunferenciaV2.java
@@ -0,0 +1,14 @@
+// Actividad 7
+import java.util.Scanner;
+
+public class CircunferenciaV2 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double radio;
+        do {
+            System.out.print("Ingresar circunferencia (0 salir): ");
+            radio = teclado.nextDouble();
+            System.out.println("Perímetro: " + (Math.PI * Math.pow(radio, 2)));
+        } while (radio != 0);
+    }
+}
