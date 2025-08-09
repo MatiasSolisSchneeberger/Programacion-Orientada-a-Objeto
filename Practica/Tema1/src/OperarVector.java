031ddd2d45be77dbd3b7bf361003e8e82e9a5c3a
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-08-09 19:31:58 -0300

    TP 1 Act 8

---
+++ Practica/Tema1/src/OperarVector.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class OperarVector {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int maxArray = 5;
+
+        int[] notas = new int[maxArray];
+        int sumatoria = 0;
+
+        for (int i = 0; i < notas.length; i++) {
+            System.out.print("Ingresar nota N° " + (i + 1) + ": ");
+            notas[i] = teclado.nextInt();
+            sumatoria = sumatoria + notas[i];
+        }
+
+        System.out.print("\nPromedio: " + (sumatoria / notas.length));
+    }
+}
