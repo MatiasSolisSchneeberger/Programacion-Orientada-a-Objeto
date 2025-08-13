60725099c5d5d6b39a9dc2685e567f2d17bc2481
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-08-13 19:51:14 -0300

    Actividad de teoría

---
+++ Teoría/Ejemplos/EjecutableAutor.java
@@ -0,0 +1,7 @@
+public class EjecutableAutor {
+	public static void main(String[] args) {
+		Autor autor1 = new Autor("José Gomez", "Facultad de Ciencias Economicas - UNNE", "jose.gomezUNNE@gmail.com");
+
+		autor1.mostrar();
+	}
+}
