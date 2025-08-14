d0067d4ba5ec31df645c748fa010dc9c8345b923
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-08-14 20:54:56 -0300

    usando todos los metodos de la clase Punto

--- Practica/Tema2/EjecutarPunto.java
+++ Practica/Tema2/EjecutarPunto.java
@@ -11,14 +11,53 @@ public class EjecutarPunto {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 
+		System.out.print("-- Punto 1 --");
 		System.out.print("Ingresar valor de X: ");
-		double x = teclado.nextDouble();
+		double x1 = teclado.nextDouble();
 
 		System.out.print("Ingresar valor de Y: ");
-		double y = teclado.nextDouble();
+		double y1 = teclado.nextDouble();
 
-		Punto punto1 = new Punto(x, y);
+		Punto punto1 = new Punto(x1, y1);
+		System.out.println(punto1.coordenadas());
+		System.out.print("- Desplazar Punto 1 -");
+		System.out.print("Ingresar valor de X: ");
+		x1 = teclado.nextDouble();
+
+		System.out.print("Ingresar valor de Y: ");
+		y1 = teclado.nextDouble();
+
+		punto1.desplazar(x1, y1);
 
 		punto1.mostrar();
+
+
+		System.out.print("-- Punto 2 --");
+		System.out.print("Ingresar valor de X: ");
+		double x2 = teclado.nextDouble();
+
+		System.out.print("Ingresar valor de Y: ");
+		double y2 = teclado.nextDouble();
+
+		Punto punto2 = new Punto(x2, y2);
+		System.out.println(punto2.coordenadas());
+
+		System.out.print("- Desplazar Punto 2 -");
+		punto2.mostrar();
+
+		System.out.print("Ingresar valor de X: ");
+		x2 = teclado.nextDouble();
+
+		System.out.print("Ingresar valor de Y: ");
+		y2 = teclado.nextDouble();
+
+		punto2.desplazar(x2, y2);
+
+		punto2.mostrar();
+
+
+		System.out.print("-- Punto 3 (en el origen) --");
+		Punto punto3 = new Punto();
+		punto3.mostrar();
 	}
 }
