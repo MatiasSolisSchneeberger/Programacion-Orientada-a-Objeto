2034b8457822dd6fc423e66f987a8b6e6c22da08
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-09-11 20:39:03 -0300

    Documentando

--- Practica/Tema4/Banco.java
+++ Practica/Tema4/Banco.java
@@ -1,13 +1,32 @@
 import java.util.ArrayList;
 
+/**
+ * TP 4 - Act 4
+ * La clase Banco representa una entidad bancaria con información sobre
+ * su nombre, número de la sucursal, localidad y lista de empleados.
+ * Permite gestionar empleados, calcular nóminas y mostrar información relacionada.
+ *
+ * @author Matias Solis Schneeberger
+ * @version 1.0
+ */
 public class Banco {
 	private String nombre;
 	private int nroSucursal;
 	private Localidad localidad;
 	private ArrayList<Empleado> empleados;
 
-	// constructor para 1 empleado
-	Banco(String p_nombre, Localidad p_localidad, int p_nroSucursal, Empleado p_empleado) {
+	/**
+	 * Construye un objeto Banco con un empleado inicial.
+	 *
+	 * @param p_nombre      Nombre del banco
+	 * @param p_localidad   Localidad donde se encuentra el banco
+	 * @param p_nroSucursal Número de identificación de la sucursal
+	 * @param p_empleado    Primer empleado asignado al banco
+	 */
+	Banco(String p_nombre,
+		  Localidad p_localidad,
+		  int p_nroSucursal,
+		  Empleado p_empleado) {
 		this.setNombre(p_nombre);
 		this.setLocalidad(p_localidad);
 		this.setNroSucursal(p_nroSucursal);
@@ -15,54 +34,121 @@ public class Banco {
 		this.agregarEmpleado(p_empleado);
 	}
 
-	// constructor para n empleados
-	Banco(String p_nombre, Localidad p_localidad, int p_nroSucursal, ArrayList<Empleado> p_empleados) {
+	/**
+	 * Construye un objeto Banco con una lista existente de empleados.
+	 *
+	 * @param p_nombre      Nombre del banco
+	 * @param p_localidad   Localidad donde se encuentra el banco
+	 * @param p_nroSucursal Número de identificación de la sucursal
+	 * @param p_empleados   Lista de empleados asignados al banco
+	 */
+	Banco(String p_nombre,
+		  Localidad p_localidad,
+		  int p_nroSucursal,
+		  ArrayList<Empleado> p_empleados) {
 		this.setNombre(p_nombre);
 		this.setLocalidad(p_localidad);
 		this.setNroSucursal(p_nroSucursal);
 		this.setEmpleados(p_empleados);
 	}
 
+	/**
+	 * Obtiene el nombre del banco.
+	 *
+	 * @return Nombre actual del banco
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
-	private void setNombre(String nombre) {
-		this.nombre = nombre;
+	/**
+	 * Establece el nombre del banco.
+	 *
+	 * @param p_nombre Nuevo nombre para el banco
+	 */
+	private void setNombre(String p_nombre) {
+		this.nombre = p_nombre;
 	}
 
+	/**
+	 * Obtiene el número de sucursal.
+	 *
+	 * @return Número de identificación de la sucursal
+	 */
 	public int getNroSucursal() {
 		return nroSucursal;
 	}
 
-	private void setNroSucursal(int nroSucursal) {
-		this.nroSucursal = nroSucursal;
+	/**
+	 * Establece el número de sucursal.
+	 *
+	 * @param p_nroSucursal Nuevo número de sucursal
+	 */
+	private void setNroSucursal(int p_nroSucursal) {
+		this.nroSucursal = p_nroSucursal;
 	}
 
+	/**
+	 * Obtiene la localidad del banco.
+	 *
+	 * @return Objeto Localidad asociado al banco
+	 */
 	public Localidad getLocalidad() {
 		return localidad;
 	}
 
-	private void setLocalidad(Localidad localidad) {
-		this.localidad = localidad;
+	/**
+	 * Establece la localidad del banco.
+	 *
+	 * @param p_localidad Nueva localidad para el banco
+	 */
+	private void setLocalidad(Localidad p_localidad) {
+		this.localidad = p_localidad;
 	}
 
+	/**
+	 * Obtiene la lista de empleados del banco.
+	 *
+	 * @return ArrayList con todos los empleados
+	 */
 	public ArrayList<Empleado> getEmpleados() {
 		return empleados;
 	}
 
-	private void setEmpleados(ArrayList<Empleado> empleaods) {
-		this.empleados = empleaods;
+	/**
+	 * Establece la lista de empleados del banco.
+	 *
+	 * @param p_empleados Nueva lista de empleados
+	 */
+	private void setEmpleados(ArrayList<Empleado> p_empleaods) {
+		this.empleados = p_empleaods;
 	}
 
+	/**
+	 * Agrega un empleado a la lista del banco.
+	 *
+	 * @param p_empleado Empleado a agregar
+	 * @return true si se agregó correctamente, false en caso contrario
+	 */
 	public boolean agregarEmpleado(Empleado p_empleado) {
 		return this.getEmpleados().add(p_empleado);
 	}
 
+	/**
+	 * Elimina un empleado de la lista del banco.
+	 *
+	 * @param p_empleado Empleado a eliminar
+	 * @return true si se eliminó correctamente, false en caso contrario
+	 */
 	public boolean quitarEmpleado(Empleado p_empleado) {
 		return this.getEmpleados().remove(p_empleado);
 	}
 
+	/**
+	 * Calcula el total de sueldos netos a pagar a todos los empleados.
+	 *
+	 * @return Sumatoria de todos los sueldos netos
+	 */
 	public double sueldoAPagar() {
 		double total = 0;
 		for (Empleado empleado : empleados) {
@@ -71,6 +157,10 @@ public class Banco {
 		return total;
 	}
 
+	/**
+	 * Muestra en consola un listado formateado de los sueldos de todos los empleados,
+	 * incluyendo CUIL, apellido y nombre, y sueldo neto.
+	 */
 	public void listarSueldos() {
 		for (Empleado empleado : empleados) {
 			System.out.printf("%-15s %-20s $%9.2f%n",
@@ -78,9 +168,15 @@ public class Banco {
 					empleado.getApellido() + ", " + empleado.getNombre(),
 					empleado.sueldoNeto());
 		}
-
 	}
 
+	/**
+	 * Muestra información completa del banco incluyendo:
+	 * - Nombre y número de la sucursal
+	 * - Detalles de la localidad
+	 * - Listado de sueldos de empleados
+	 * - Total general de sueldos a pagar
+	 */
 	public void mostrar() {
 		System.out.println("Banco: " + this.getNombre() +
 				"\tSucursal: " + this.getNroSucursal());
@@ -94,4 +190,3 @@ public class Banco {
 				this.sueldoAPagar());
 	}
 }
-
